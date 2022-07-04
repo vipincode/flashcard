19ca3c0f2be1cc025ae9a3c1df8df212dde2aed2
Vipin Singh <svipin174@gmail.com>
2022-07-04 21:22:24 +0530

    craeting color loop in pug

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const { request, response } = require('express');
 const express = require('express');
 
-// This app is central part of our application.
-// We exted it by adding routes, middleware and other settings
 const app = express();
 
-// Lets use pug.
-// App.ser define different setting in express.
-// This line tells express which template engine to use.
-// by default look in the views folder for template.
+const colors = ['red', 'green', 'orange', 'blue', 'purple'];
+
 app.set('view engine', 'pug');
 
 // Routes
@@ -20,13 +16,10 @@ app.get('/cards', (req, res) => {
   res.render('card', {
     prompt: "Who is burried in grunt's tomb?",
     hint: 'Think about who is tomb it is?',
+    colors,
   });
-  //  OR
-  // res.locals.prompt = "Who is burried in grunt's tomb?";
-  // res.render('card'); card is pug name see in views folder
 });
 
-// Lets detup development server
 app.listen(3000, () => {
   console.log('The application is running on PORT: 3000!');
 });
