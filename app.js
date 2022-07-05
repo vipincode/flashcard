c220e4436b71fc106c025e07bc1f9edea61a54ef
Vipin Singh <svipin174@gmail.com>
2022-07-05 08:57:43 +0530

    fix: post request basics

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const { request, response } = require('express');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// LET USE BODY PARSER
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// USE PUF TEMPLATE
 app.set('view engine', 'pug');
 
-// Routes
+// ROUTES
 app.get('/', (req, res) => {
   res.render('index');
 });
@@ -15,7 +19,17 @@ app.get('/cards', (req, res) => {
     hint: 'Think about who is tomb it is?',
   });
 });
+app.get('/hello', (req, res) => {
+  res.render('hello');
+});
 
+app.post('/hello', (req, res) => {
+  // console.dir(req.body);
+  res.render('hello', { name: req.body.username });
+  // res.json(req.body);
+});
+
+// SERVER LISTENING
 app.listen(3000, () => {
   console.log('The application is running on PORT: 3000!');
 });
